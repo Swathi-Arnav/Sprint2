2a69582878a9d36af01a13e54122753183f69d8c
N <swathi.b.n@capgemini.com>
2023-02-20 21:34:41 +0530

    added test cases and utilst

--- src/test/java/runners/TestRunners.java
+++ src/test/java/runners/TestRunners.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = "src/test/resources/features",glue = "Steps",publish = true
-        ,plugin = { "pretty", "html:target/cucumber-reports.html" ,"html:target/cucumber-reports.json" },
+        ,plugin = { "pretty", "html:target/cucumber-reports.html" ,"json:target/cucumber-reports.json" },
         monochrome = true)
 
 public class TestRunners extends AbstractTestNGCucumberTests{
